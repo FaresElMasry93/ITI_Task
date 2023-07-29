c3643013b1389a9925768cbd5563fedad74ed915
FaresElMasry93 <fareselmasry1251993@gmail.com>
2023-07-29 12:36:44 +0300

    first commit

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,26 +1,43 @@
-package Pages;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
-public class LoginPage {
-    private WebDriver driver;
-    private By usernameField = By.id("username");
-    private By passwordField = By.id("password");
-    private By loginButton = By.cssSelector("#login button");
+package pages;
 
-    public LoginPage(WebDriver driver){
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginPage {
+
+    public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void setUsername(String username){
-        driver.findElement(usernameField).sendKeys(username);
+    private WebDriver driver;
+    private By Email = By.id("edit-name");
+    private By Password = By.id("edit-pass");
+    private By Submit = By.id("edit-submit");
+    private By SearchBar = By.name("search");
+
+    public void setEmail(String username){
+
+        driver.findElement(Email).sendKeys(username);
     }
 
     public void setPassword(String password){
-        driver.findElement(passwordField).sendKeys(password);
+
+        driver.findElement(Password).sendKeys(password);
+    }
+    public void submit(){
+
+        driver.findElement(Submit).click();
     }
 
-    public SecureAreaPage clickLoginButton(){
-        driver.findElement(loginButton).click();
-        return new SecureAreaPage(driver);
-    }
+
+     public Favourites searchText(String SearchText) {
+         driver.findElement(SearchBar).sendKeys(SearchText);
+      return new Favourites(driver);
+
+   }
+
+
+
+
 }
